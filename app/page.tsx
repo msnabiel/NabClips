bf9ff01c88f31b25bdb006d9d87a44b3c3d4e529
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-04-29 19:52:41 +0530

    Bugs zapped & names revamped 🎯

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Features from "@/components/features";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import { Navbar } from "@/components/navbar";
-import Pricing from "@/components/pricing";
 import Testimonial from "@/components/testimonial";
 
 export default function Home() {
