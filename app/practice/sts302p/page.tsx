1752a926f10599fd0eec9d19b72188d4b582cdb7
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-05-04 23:22:47 +0530

    Bugs zapped & names revamped 🎯

---
+++ app/practice/sts302p/page.tsx
@@ -0,0 +1,40 @@
+import FAQ from "@/components/faq";
+import Features from "@/components/features";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import { Navbar } from "@/components/navbar";
+import Testimonial from "@/components/testimonial";
+//import { Card } from "@/components/ui/card";
+//import { Button } from "@/components/ui/button";
+{/*
+{/* Code Retrieval Section
+<Card className="p-6 space-y-4">
+<div>
+  <h2 className="text-xl font-semibold mb-3">Retrieve Saved Clip by Code</h2>
+  <div className="flex gap-3 items-center">
+    <input
+      type="text"
+      value={inputCode}
+      onChange={(e) => setInputCode(e.target.value)}
+      placeholder="Enter 4-digit code"
+      className="border px-3 py-2 text-base rounded-md h-10"
+      maxLength={4}
+    />
+    <Button onClick={handleRetrieve} variant="outline" className="h-10">
+      Retrieve
+    </Button>
+  </div>
+</div>
+</Card>*/}
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <FAQ />
+      <Testimonial />
+      <Footer />
+    </>
+  );
+}
