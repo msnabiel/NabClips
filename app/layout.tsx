b0a9059dfb47769b666a81e8d9cc5050e0abfc45
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-05-04 23:17:58 +0530

    Bugs zapped & names revamped 🎯

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ const geistSans = Geist({
 });
 
 export const metadata: Metadata = {
-  title: "Beautiful Shadcn UI Landing Page",
+  title: "Nabprep- Your STS Prep Companion",
   description:
-    "A beautiful landing page built with Shadcn UI, Next.js 15, Tailwind CSS, and Shadcn UI Blocks.",
+    "Practice curated MCQs, track your progress, and master the STS syllabus with focused study material—all built by a VIT student for VIT students.",
   keywords: [
     "Shadcn UI Landing Page",
     "Shadcn UI Blocks",
