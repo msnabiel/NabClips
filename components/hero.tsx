0b92a311f9ac42a0f838095414b127a2db332284
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-05-05 08:43:49 +0530

    Bugs zapped & names revamped 🎯

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { ArrowUpRight, CirclePlay, FileText } from "lucide-react";
+import { ArrowUpRight, FileText } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
